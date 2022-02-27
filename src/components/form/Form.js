9074a17ca9d2e3e04098400637cf61c15befa6ff
André Perdigão <andrepcg@gmail.com>
2022-02-27 22:06:01 +0000

    Identify people

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,19 @@
 import { useRef } from 'react';
 
+import { identify } from '@sqill/utils/gtag';
 import { StyledForm, Div, Label, Input, Button } from './Form.styles';
+
+function identifyUserFromFields(data) {
+  if (!data) return;
+
+  const email = data.email || data.Email;
+  const name = data.name || data.Name;
+
+  if (!email && !name) return;
+
+  identify({ email, name });
+}
+
 
 const Form = ({ fields, button, tableName, success, error }) => {
   const formRef = useRef();
@@ -8,6 +21,8 @@ const Form = ({ fields, button, tableName, success, error }) => {
   const onFormSubmit = async (ev) => {
     ev.preventDefault();
     const data = Object.assign({}, ...fields.map((field) => ({ [field.id]: document.getElementById(field.id).value })));
+
+    identifyUserFromFields(data);
 
     await fetch('/.netlify/functions/form-submit',
       {
