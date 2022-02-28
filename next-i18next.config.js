03f9e36eca9571489bcd608c2180e301b9e52e29
André Perdigão <andrepcg@gmail.com>
2022-02-28 08:53:23 +0000

    Set default locale to en

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     locales: ['pt', 'en'],
-    defaultLocale: 'pt',
+    defaultLocale: 'en',
     fallbackLng: 'pt',
     localeDetection: false,
     preload: true,
